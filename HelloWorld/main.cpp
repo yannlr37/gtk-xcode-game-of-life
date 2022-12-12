141962ebfd6f02f0aae7a92ededa90277736e4ee
yann <yann@mbp-de-yann.lan>
2022-12-12 08:07:21 +0100

    Initial Commit

---
+++ HelloWorld/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by yann on 12/12/2022.
+//  Copyright (c) 2022 yann. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
